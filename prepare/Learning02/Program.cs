8031413c5b753d2edd06cbca0824e0f2fc717446
Greice Moreira <greice.f.d.a@hotmail.com>
2024-09-25 11:06:06 -0400

    abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Beneva";
+        job1._jobTitle = "Web Developper";
+        job1._startYear = 2025 ;
+        job1._endYear = 2028;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Web Developper";
+        job2._startYear = 2028 ;
+        job2._endYear = 2035;
+
+        Resume myResume = new Resume();
+        myResume._userName = "Greice";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
+
     }
 }
